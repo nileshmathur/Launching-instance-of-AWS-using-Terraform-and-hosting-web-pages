bf6a75d0178333a028aa5dae482f9c78c6ca87b5
nileshmathur <64744992+nileshmathur@users.noreply.github.com>
2020-06-12 00:51:55 +0530

    Create mywebpage.php

---
+++ mywebpage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>Welcome to my Website!!!</title>
+</head>
+<body>
+    <img src='https://a0.awsstatic.com/main/images/logos/aws_logo_smile_1200x630.png'>
+</body>
+</html>
+<br/>
+<?php>
+print 'date'
+print 'ifconfig'
+
+
+This is a website made just for testing the AWS instance!!!!
